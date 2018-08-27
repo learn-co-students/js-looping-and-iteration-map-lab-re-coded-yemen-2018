dbca8af1d81a1e084bd22d53db0e5d6fdcb5e125
Salah Al-Dhaferi <binaryminded@gmail.com>
2018-08-27 09:47:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  let newArray = [];
+  
+  for (let element of array){
+    newArray.push(element.toLowerCase());
+  }
+  return newArray;
+}
+function nameToAttributes(array){
+  let newArray=[];
+  for (let element of array){
+    let firstName = element.split(" ")[0];
+    let lastName = element.split(" ")[1];
+    
+    newArray.push({firstName: firstName, lastName: lastName});
+  }
+  return newArray;
+  
+}
+
+function attributesToPhrase(array){
+  let newArray=[];
+  for (let element of array){
+    newArray.push(`${element.name} is from ${element.hometown}`);
+    
+  }
+  
+  return newArray;
+}
+
+
+
+
+
